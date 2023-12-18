56d9175fe5344cc1322c23ae5c45aac5bef18648
bktpp <117415722+bktpp@users.noreply.github.com>
2023-12-18 14:12:50 -0600

    updated contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,9 @@
-import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
-import { ToastContainer, toast } from "react-toastify";
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
 
-import "react-toastify/dist/ReactToastify.css";
-import { Tooltip } from "./Tooltip";
+import 'react-toastify/dist/ReactToastify.css';
+import { Tooltip } from './Tooltip';
 
 const Contact = ({ classicHeader, darkTheme }) => {
    const form = useRef();
@@ -13,33 +13,33 @@ const Contact = ({ classicHeader, darkTheme }) => {
       e.preventDefault();
       setSendingMail(true);
       emailjs
-         .sendForm("service_8d2adox", "template_atob7al", form.current, "Ad-2ojkoRPjt5_Sac")
+         .sendForm('service_8d2adox', 'template_atob7al', form.current, 'Ad-2ojkoRPjt5_Sac')
          .then(
             (result) => {
-               document.getElementById("contact-form").reset();
-               toast.success("Message sent successfully!", {
-                  position: "top-right",
+               document.getElementById('contact-form').reset();
+               toast.success('Message sent successfully!', {
+                  position: 'top-right',
                   autoClose: 5000,
                   hideProgressBar: false,
                   closeOnClick: true,
                   pauseOnHover: true,
                   draggable: true,
                   progress: undefined,
-                  theme: darkTheme ? "dark" : "light",
+                  theme: darkTheme ? 'dark' : 'light',
                });
                console.log(result.text);
                setSendingMail(false);
             },
             (error) => {
-               toast.error("Something went wrong!", {
-                  position: "top-right",
+               toast.error('Something went wrong!', {
+                  position: 'top-right',
                   autoClose: 5000,
                   hideProgressBar: false,
                   closeOnClick: true,
                   pauseOnHover: true,
                   draggable: true,
                   progress: undefined,
-                  theme: darkTheme ? "dark" : "light",
+                  theme: darkTheme ? 'dark' : 'light',
                });
                console.log(error.text);
                setSendingMail(false);
@@ -48,25 +48,25 @@ const Contact = ({ classicHeader, darkTheme }) => {
    };
 
    return (
-      <section id="contact" className={"section " + (darkTheme ? "bg-dark-2" : "bg-light")}>
-         <div className={"container " + (classicHeader ? "" : "px-lg-5")}>
+      <section id="contact" className={'section ' + (darkTheme ? 'bg-dark-2' : 'bg-light')}>
+         <div className={'container ' + (classicHeader ? '' : 'px-lg-5')}>
             {/* Heading */}
             <div className="position-relative d-flex text-center mb-5">
                <h2
                   className={
-                     "text-24  text-uppercase fw-600 w-100 mb-0 " +
-                     (darkTheme ? "text-muted opacity-1" : "text-light opacity-4")
+                     'text-24  text-uppercase fw-600 w-100 mb-0 ' +
+                     (darkTheme ? 'text-muted opacity-1' : 'text-light opacity-4')
                   }
                >
                   Contact
                </h2>
                <p
                   className={
-                     "text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0 " +
-                     (darkTheme ? "text-white" : "text-dark")
+                     'text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0 ' +
+                     (darkTheme ? 'text-white' : 'text-dark')
                   }
                >
-                  {" "}
+                  {' '}
                   Get in Touch
                   <span className="heading-separator-line border-bottom border-3 border-primary d-block mx-auto" />
                </p>
@@ -75,17 +75,17 @@ const Contact = ({ classicHeader, darkTheme }) => {
             <div className="row gy-5">
                {/* contact details */}
                <div className="col-md-4 col-xl-3 order-1 order-md-0 text-center text-md-start">
-                  <h2 className={"mb-3 text-5 text-uppercase " + (darkTheme ? "text-white" : "")}>
-                     Address
+                  <h2 className={'mb-3 text-5 text-uppercase ' + (darkTheme ? 'text-white' : '')}>
+                     Contact
                   </h2>
-                  <p className={"text-3 mb-4 " + (darkTheme ? "text-light" : "")}>
-                     1506 Iris Ave.
-                     <br />
-                     Killeen, TX
-                     {/* <br />
+                  {/* <p className={'text-3 mb-4 ' + (darkTheme ? 'text-light' : '')}>
+                     Kileen, TX */}
+                  {/* <br />
+                     Killeen, TX */}
+                  {/* <br />
               San Diego CA 2028 */}
-                  </p>
-                  <p className={"text-3 mb-1 " + (darkTheme ? "text-light" : "")}>
+                  {/* </p> */}
+                  <p className={'text-3 mb-1 ' + (darkTheme ? 'text-light' : '')}>
                      <span className="text-primary text-4 me-2">
                         <i className="fas fa-phone" />
                      </span>
@@ -97,19 +97,19 @@ const Contact = ({ classicHeader, darkTheme }) => {
                      </span>
                      (060) 555 545 555
                   </p> */}
-                  <p className={"text-3 mb-4 " + (darkTheme ? "text-light" : "")}>
+                  <p className={'text-3 mb-4 ' + (darkTheme ? 'text-light' : '')}>
                      <span className="text-primary text-4 me-2">
                         <i className="fas fa-envelope" />
                      </span>
                      bktp@protonmail.ch
                   </p>
-                  <h2 className={"mb-3 text-5 text-uppercase " + (darkTheme ? "text-white" : "")}>
+                  <h2 className={'mb-3 text-5 text-uppercase ' + (darkTheme ? 'text-white' : '')}>
                      Follow Me
                   </h2>
                   <ul
                      className={
-                        "social-icons justify-content-center justify-content-md-start " +
-                        (darkTheme ? "social-icons-muted" : "")
+                        'social-icons justify-content-center justify-content-md-start ' +
+                        (darkTheme ? 'social-icons-muted' : '')
                      }
                   >
                      {/* <li className="social-icons-dribbble">
@@ -167,20 +167,20 @@ const Contact = ({ classicHeader, darkTheme }) => {
                            </a>
                         </Tooltip>
                      </li>
-                  </ul>{" "}
+                  </ul>{' '}
                </div>
                {/* contact form */}
                <div className="col-md-8 col-xl-9 order-0 order-md-1">
                   <h2
                      className={
-                        "mb-3 text-5 text-uppercase text-center text-md-start " +
-                        (darkTheme ? "text-white" : "")
+                        'mb-3 text-5 text-uppercase text-center text-md-start ' +
+                        (darkTheme ? 'text-white' : '')
                      }
                   >
                      Send me a note
                   </h2>
                   <form
-                     className={darkTheme ? "form-dark" : ""}
+                     className={darkTheme ? 'form-dark' : ''}
                      id="contact-form"
                      action="php/mail.php"
                      method="post"
@@ -213,7 +213,7 @@ const Contact = ({ classicHeader, darkTheme }) => {
                               rows={5}
                               required
                               placeholder="Tell me more about your questions........"
-                              defaultValue={""}
+                              defaultValue={''}
                            />
                         </div>
                      </div>
